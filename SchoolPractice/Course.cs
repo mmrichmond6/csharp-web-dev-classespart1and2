3b56810f4467eda217dfb35ba100576adaaf5b00
mmrichmond6 <mmrichmond6@gmail.com>
2022-08-19 10:40:04 -0500

    I think I'm done but not completely understanding the equals

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace SchoolPractice
 {
@@ -18,17 +19,42 @@ namespace SchoolPractice
         }
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather than
         //  just the class fields.
-
-
-
-        public override bool Equals(object obj)
+        public override string ToString()
         {
-            return obj is Course course &&
-                   Topic == course.Topic &&
-                   EqualityComparer<Teacher>.Default.Equals(Instructor, course.Instructor);
+            return Instructor + " (Course topic: " + Topic + ", Students Enrolled: " + EnrolledStudents + ")";
         }
+
+
+        //public override bool Equals(object obj)
+        //{
+            //return obj is Course course &&
+                   //Topic == course.Topic &&
+                   //EqualityComparer<Teacher>.Default.Equals(Instructor, course.Instructor);
+        //}
 
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Course objects equal.
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            //return obj is Course course &&  Topic == course.Topic &&
+            //EqualityComparer<Teacher>.Default.Equals(Instructor, course.Instructor);
+            Course courseObj = obj as Course;
+            return Instructor == courseObj.Instructor && Topic == courseObj.Topic;
+        }
+
     }
 }
